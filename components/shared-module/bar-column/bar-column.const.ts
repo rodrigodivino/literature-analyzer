51a2e1b7b48de979b050d095b9688eb812880e9f
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-02-09 11:31:39 -0300

    Add pan scroll functionality

--- components/shared-module/bar-column/bar-column.const.ts
+++ components/shared-module/bar-column/bar-column.const.ts
@@ -2,11 +2,14 @@ import {BarColumnTypes} from "./bar-column.types";
 
 export namespace BarColumnConst {
   export const MARGIN = {TOP: 20, LEFT: 20, RIGHT: 20, BOTTOM: 20};
-  export const CELL_HEIGHT = 80;
   export const BAR_HEIGHT = 30;
+  export const LABEL_HEIGHT = 30;
+  export const BAR_PADDING = 10;
+  export const CELL_HEIGHT = BarColumnConst.BAR_HEIGHT + BarColumnConst.LABEL_HEIGHT + BarColumnConst.BAR_PADDING;
+  
   export const VALUE_LABEL_WIDTH = 30;
   
-  export const TEST_DATA: Array<BarColumnTypes.Datum> = new Array(5)
+  export const TEST_DATA: Array<BarColumnTypes.Datum> = new Array(35)
       .fill(0)
       .map((d, i) => ({label: `Bar Nº ${i}`, value: i * 2, id: `Bar Nº ${i}`}));
 }
--- components/shared-module/bar-column/bar-column.tsx
+++ components/shared-module/bar-column/bar-column.tsx
@@ -1,28 +1,75 @@
-import {FunctionComponent} from "react";
+import {FunctionComponent, useEffect, useRef, useState} from "react";
 import {BarColumnTypes} from "./bar-column.types";
 import {getMarginConvention} from "../../../hooks/shared-module/get-margin-convention";
 import {BarColumnConst} from "./bar-column.const";
-import styles from './bar-column.module.css'
-import {max, scaleLinear} from "d3";
+import styles from './bar-column.module.css';
+import {max, scaleLinear, select, zoom, ZoomBehavior, zoomIdentity, ZoomTransform} from "d3";
+import {useWrapperState} from "../../../hooks/shared-module/use-wrapper-state";
 
 export const BarColumn: FunctionComponent<BarColumnTypes.Props> = ({width, height, data, color}) => {
   const [innerWidth, innerHeight, translate] = getMarginConvention(width, height, BarColumnConst.MARGIN);
   
-  const scale = scaleLinear<number, number>().domain([0, max(data, d => d.value) ?? 0]).range([0, innerWidth - BarColumnConst.VALUE_LABEL_WIDTH])
+  const scale = scaleLinear<number, number>()
+      .domain([0, max(data, d => d.value) ?? 0])
+      .range([0, innerWidth - BarColumnConst.VALUE_LABEL_WIDTH]);
   
-  return <g transform={translate}>
+  const g = useRef<SVGGElement>(null);
+  
+  
+  const [transform, setTransform] = useState<ZoomTransform>(zoomIdentity);
+  const [zoomBehavior, setZoomBehavior] = useWrapperState<ZoomBehavior<SVGGElement, unknown>>();
+  
+  useEffect(() => {
+    if (!g.current) {
+      return;
+    }
+    
+    const zoomBehavior = zoom<SVGGElement, unknown>()
+        .on('zoom', (event) => {
+          setTransform(event.transform);
+        });
+  
+  
+    select(g.current).call(zoomBehavior)
+        .on('wheel.zoom', null)
+        .on('wheel', (event) => {
+          if(!g.current) return;
+          select(g.current).call(zoomBehavior.translateBy, event.wheelDeltaX * 0.4, event.wheelDeltaY * 0.4)
+        });
+    
+    setZoomBehavior(zoomBehavior);
+  }, [g, setZoomBehavior]);
+  
+  useEffect(() => {
+    if (!zoomBehavior) {
+      return;
+    }
+    
+    const worldHeight = BarColumnConst.CELL_HEIGHT * data.length;
+    
+    zoomBehavior
+        .scaleExtent([1, 1])
+        .extent([[0, 0], [innerWidth, innerHeight]])
+        .translateExtent([[0, 0], [innerWidth, worldHeight]]);
+    
+  }, [zoomBehavior, innerWidth, innerHeight, data]);
+  
+  return <g ref={g} transform={translate}>
     {data.map((d, i) => {
-      return <g key={d.id} transform={`translate(0,${i * BarColumnConst.CELL_HEIGHT})`}>
+      return <g shapeRendering={'crispEdges'} key={d.id}
+                transform={`translate(0,${i * BarColumnConst.CELL_HEIGHT + transform.y})`}>
         <rect className={styles.cellBackground} width={innerWidth} height={BarColumnConst.CELL_HEIGHT}/>
-        <rect className={styles.cellBar} style={{fill: color}} width={scale(d.value)} y={BarColumnConst.CELL_HEIGHT / 2} height={BarColumnConst.BAR_HEIGHT}/>
-        <foreignObject width={innerWidth} height={BarColumnConst.CELL_HEIGHT / 2}>
+        <rect className={styles.cellBar} style={{fill: color}} width={scale(d.value)} y={BarColumnConst.LABEL_HEIGHT}
+              height={BarColumnConst.BAR_HEIGHT}/>
+        <foreignObject width={innerWidth} height={BarColumnConst.LABEL_HEIGHT}>
           <div className={styles.labelOuter}>
             <div className={styles.labelInner}>
               {d.label}
             </div>
           </div>
         </foreignObject>
-        <foreignObject x={scale(d.value)} y={BarColumnConst.CELL_HEIGHT / 2} width={BarColumnConst.VALUE_LABEL_WIDTH} height={BarColumnConst.BAR_HEIGHT}>
+        <foreignObject x={scale(d.value)} y={BarColumnConst.LABEL_HEIGHT} width={BarColumnConst.VALUE_LABEL_WIDTH}
+                       height={BarColumnConst.BAR_HEIGHT}>
           <div className={styles.valueLabelOuter}>
             <div className={styles.valueLabelInner}>
               {d.value}
