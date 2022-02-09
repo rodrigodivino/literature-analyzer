366eaad0591f765a2e3f3e2362d248f692a95198
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-02-09 09:45:06 -0300

    Prepare generic props in widget column

--- shared/widget-column/widget-column.tsx
+++ shared/widget-column/widget-column.tsx
@@ -1,22 +1,19 @@
-import {FunctionComponent} from "react";
 import {getMarginConvention} from "../hooks/get-margin-convention";
 import {WidgetColumnConst} from "./widget-column.const";
 import {WidgetColumnTypes} from "./widget-column.types";
 
 // TODO: Continue from here with widget generics experiments
 
-export const WidgetColumn: FunctionComponent<WidgetColumnTypes.Props<unknown>> = <T,>(
+export const WidgetColumn = <T,>(
     {
       width,
       height,
-      widget,
-      widgetData
+      data
     }: WidgetColumnTypes.Props<T>
 ) => {
   
   const [innerWidth, innerHeight, translate] = getMarginConvention(width, height, WidgetColumnConst.MARGIN);
   
-  const data = [{}, {}];
   return <g transform={translate}>
     <rect width={innerWidth} height={innerHeight}/>
   </g>;
--- shared/widget-column/widget-column.types.ts
+++ shared/widget-column/widget-column.types.ts
@@ -1,10 +1,12 @@
-import {FunctionComponent, ReactComponentElement, ReactElement} from "react";
+import {ReactElement} from "react";
 
 export namespace WidgetColumnTypes {
   export interface Props<WidgetData> {
     width: number;
     height: number;
-    widget: ReactElement<WidgetData>;
-    widgetData: WidgetData;
+    data: {
+      widgetData: WidgetData[],
+      widgetComponent: ReactElement<WidgetData>
+    };
   }
 }
