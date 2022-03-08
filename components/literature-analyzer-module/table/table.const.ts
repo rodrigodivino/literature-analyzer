2cc77e1f23608ef5e4cdf243260156df00502ccc
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-03-08 15:32:47 -0300

    Fix type errors

--- components/literature-analyzer-module/table/table.const.ts
+++ components/literature-analyzer-module/table/table.const.ts
@@ -1,16 +1,3 @@
-import {TableTypes} from "./table.types";
-import {BibTexStats, KeywordStats} from "../../../hooks/literature-analyzer-module/get-bibtex-stats";
-
 export namespace TableConst {
-  export const ROW_HEIGHT: number = 30;
   export const MARGIN = {TOP: 20, LEFT: 20, RIGHT: 20, BOTTOM: 20};
-  
-  export const COLUMN_TO_KEY = new Map<TableTypes.ColumnType, keyof KeywordStats>(
-      [
-        [TableTypes.ColumnType.SURVEY, 'occurrencesInSurveys'],
-        [TableTypes.ColumnType.OCCURRENCES, 'occurrencesInRecent'],
-        [TableTypes.ColumnType.KEYWORD, 'keyword'],
-        [TableTypes.ColumnType.TREND, 'occurrencesOverTime'],
-      ]
-  );
 }
--- components/shared-module/trend-cell/trend-cell.const.ts
+++ components/shared-module/trend-cell/trend-cell.const.ts
@@ -5,7 +5,7 @@ export namespace TrendCellConst {
   
   export const SAMPLE_DATA: TrendCellTypes.Props = {
     contextData: [[{time: 0, value: 2, d: {}}, {time: 1, value: 3, d: {}}, {time: 2, value: 4, d: {}}]],
-    highlightedData: [[{time: 0, value: 0, d: {}}, {time: 1, value: 2, d: {}}, {time: 2, value: 5, d: {}}]],
+    highlightedData: [{time: 0, value: 0, d: {}}, {time: 1, value: 2, d: {}}, {time: 2, value: 5, d: {}}],
     timeDomain: [0, 2],
     valueDomain: [0, 5],
     width: 100,
