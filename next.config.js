b10d8e7c73efb0c361ef1a234ddfebf0ccf846cf
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-03-08 15:31:04 -0300

    Prepare for deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'development' ? '' : '/literature-analyzer',
+  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/literature-analyzer/'
 }
